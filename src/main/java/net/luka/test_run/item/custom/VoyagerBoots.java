d21f789da4708430e620dd35178925fa0c886838
LukaKanjo <Luka.kanjo2003@gmail.com>
2024-08-16 22:44:48 +0200

    Tried updating boots
    Didnt do much am trying mor tomorrow

--- src/main/java/net/luka/test_run/item/custom/VoyagerBoots.java
+++ src/main/java/net/luka/test_run/item/custom/VoyagerBoots.java
@@ -3,60 +3,36 @@ package net.luka.test_run.item.custom;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class VoyagerBoots extends Item
 {
-    public VoyagerBoots(Properties pProperties) {super(pProperties);}
+    public VoyagerBoots(Properties pProperties)
+    {
+        super(pProperties);
+    }
 
     @Override
-    public InteractionResult useOn(UseOnContext pContext)
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand)
     {
-        if (!pContext.getLevel().isClientSide())
+        if (pPlayer.hasImpulse)
         {
-            BlockPos positionClicked = pContext.getClickedPos();
-            Player player = pContext.getPlayer();
-            boolean foundblock = false;
+         if (!pLevel.isClientSide)
+         {
 
-            for (int i = 0; i <= positionClicked.getY() + 64; i++)
-            {
-                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
-
-                if (isValueableBlock(state))
-                {
-                    outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
-                    foundblock = true;
-
-                    break;
-                }
-            }
-            if (!foundblock)
-            {
-                player.sendSystemMessage(Component.literal("Nothing Found"));
-            }
-
+         }
         }
-
-        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
-                player -> player.broadcastBreakEvent(player.getUsedItemHand()));
-
-        return InteractionResult.SUCCESS;
-    }
-
-    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block)
-    {
-        player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
-                "(" + blockPos.getX() + ", " + blockPos.getY() + " , " + blockPos.getZ() + ")"));
-    }
-
-    private boolean isValueableBlock(BlockState state)
-    {
-        return state.is(Blocks.IRON_ORE);
+        return InteractionResult.SUCCESS
     }
 }
+
