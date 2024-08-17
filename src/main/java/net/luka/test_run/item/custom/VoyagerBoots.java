6d23a75513d2d850397654582eb6eda328bbca29
LukaKanjo <Luka.kanjo2003@gmail.com>
2024-08-17 14:11:47 +0200

    Made progress
    The boots now give you the speed effect when right clicking, will work on making it turn on/off later, as it seems that that will need custom tags which is not yet on of my competencies.

--- src/main/java/net/luka/test_run/item/custom/VoyagerBoots.java
+++ src/main/java/net/luka/test_run/item/custom/VoyagerBoots.java
@@ -6,14 +6,21 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 public class VoyagerBoots extends Item
 {
@@ -25,14 +32,18 @@ public class VoyagerBoots extends Item
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand)
     {
-        if (pPlayer.hasImpulse)
-        {
-         if (!pLevel.isClientSide)
-         {
+        ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
 
-         }
-        }
-        return InteractionResult.SUCCESS
+        pPlayer.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,20,2));
+
+        return InteractionResultHolder.sidedSuccess(itemStack, pLevel.isClientSide());
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack)
+    {
+        return UseAnim.SPEAR;
     }
 }
+
 
